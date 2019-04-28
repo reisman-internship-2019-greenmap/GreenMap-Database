53073c4d542942d533e0b22fc30751ef30357ff4
Mac <39278228+Reithger@users.noreply.github.com>
2019-04-28 17:19:26 -0300

    Comments

--- SPARQL Explicit Query Example.js
+++ SPARQL Explicit Query Example.js
@@ -4,9 +4,8 @@ console.log('results')
 
 const wdk = require('wikidata-sdk');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-var request = require('request');
 
-const url = wdk.searchEntities({
+const url = wdk.searchEntities({    //Can further define search parameters explicitly
   search: "Ingmar Bergman",
   format: "json",
   language: "en",
@@ -19,17 +18,17 @@ xmlHttp.open("GET", url, false);
 xmlHttp.send(null);
 console.log(url);
 console.log(xmlHttp.responseText);
-console.log(' ');
-
+console.log(' ');                       //All of this is the same as Wikidata Access Example.js
+                                        //Here we can define a SPARQL query
 var SPARQL = `
   SELECT ?child WHERE{
     ?child wdt:P178 wd:Q312.
   }`
 
-var url2 = wdk.sparqlQuery(SPARQL);
-xmlHttp.open("GET", url2, false);
+var url2 = wdk.sparqlQuery(SPARQL);   //Generate the URL from an explicit SPARQL query
+xmlHttp.open("GET", url2, false);     //Send the http request
 xmlHttp.send(null);
-console.log(url2);
+console.log(url2);                    //Print the URL and results, both simplified and not.
 console.log(xmlHttp.responseText);
 console.log(wdk.simplify.sparqlResults(xmlHttp.responseText));
 console.log(' ');
--- Wikidata Access Example.js
+++ Wikidata Access Example.js
@@ -1,14 +1,14 @@
 console.log('word');
 console.log('another')
-console.log('results')
+console.log('results')    //test print statements
 
-const wdk = require('wikidata-sdk');
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const wdk = require('wikidata-sdk');    //include wikidata-sdk into the project
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;  //include xmlhttprequest into the project
 
-const url = wdk.searchEntities('Ingmar Bergman');
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", url, false);
+const url = wdk.searchEntities('Ingmar Bergman');   //compose a wikidata search for 'Ingmar Bergman' (the url address)
+var xmlHttp = new XMLHttpRequest();                 //get the object to make URL http request
+xmlHttp.open("GET", url, false);              //make the url request, retrieve results
 xmlHttp.send(null);
-console.log(url);
+console.log(url);                             //print the URL searched, and the returned results
 console.log(xmlHttp.responseText);
 console.log(' ');
